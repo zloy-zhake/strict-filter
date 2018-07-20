f808e48a10c87108760744e9f54c7c769cc9bfaa
Zhandos Zhumanov <jk-jdogg@yandex.ru>
2018-07-20 23:17:28 +0600

    Добавил обработку параметров командной строки

--- strict-filter.py
+++ strict-filter.py
@@ -8,9 +8,9 @@ Usage:
 
 Options:
   -h --help                 вывести справку
-  -i --input-file           (TODO) по умолчанию - stdin
-  -o --output-file          (TODO) по умолчанию - stdout
-  --filters-path            (TODO) путь до папки, в которой находятся файлы с фильтрами (по умолчанию - ./filters)
+  -i --input-file           по умолчанию - stdin
+  -o --output-file          по умолчанию - stdout
+  --filters-path            путь до папки, в которой находятся файлы с фильтрами (по умолчанию - ./filters)
   --filters-pick            (TODO) список применяемых фильтров (если не указан, применяются все фильтры из --filters-path)
   --interactive             (TODO) интерактивный режим, в котором для каждого символа, который не проходит фильтры, задаётся вопрос пользователю. (Для создания и проверки фильтров)
   --interactive-new-filter  (TODO) интерактивный режим для создания нового фильтра
@@ -18,9 +18,29 @@ Options:
 """
 
 from docopt import docopt
+from sys import stdin, stdout
+from os import getcwd
 
+# преобразование параметров командной строки в dict
 arguments = docopt(__doc__)
-print(arguments)
+# print(arguments)
+
+# разбираем параметры командной строки
+if arguments["--input-file"]:
+  input_file = arguments["INPUT_FILE"]
+else:
+  input_file = stdin
+
+if arguments["--output-file"]:
+  output_file = arguments["OUTPUT_FILE"]
+else:
+  output_file = stdout
+
+if arguments["--filters-path"]:
+  filter_path = arguments["PATH"]
+else:
+  filter_path = getcwd() + "/filters"
+
 
 
 # прочитать строку из файла
