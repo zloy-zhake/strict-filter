a6781bff7b7622127f3e84613d364b849f7e76cf
Zhandos Zhumanov <jk-jdogg@yandex.ru>
2018-07-21 00:53:58 +0600

    Реализовал фильтрацию без интерактивного режима

--- strict-filter.py
+++ strict-filter.py
@@ -1,58 +1,74 @@
 """
+==========
 strict-filter.py - строгий фильтр, который пропускает только разрешённые символы, перечисленные в фильтрах.
 Фильтры - файлы с расширением .fltr
 
 Usage:
-  strict-filter.py (-h | --help)
-  strict-filter.py [-i INPUT_FILE | --input-file INPUT_FILE] [-o OUTPUT_FILE | --output-file OUTPUT_FILE] [--filters-path PATH] [--filters-pick FILTER ...] [--interactive] [--interactive-new-filter]
+    strict-filter.py [-i INPUT_FILE | --input-file INPUT_FILE] [-o OUTPUT_FILE | --output-file OUTPUT_FILE] [--filters-path PATH] [--filters-pick FILTER ...] [--interactive] [--interactive-new-filter]
+    strict-filter.py (-h | --help)
 
 Options:
-  -h --help                 вывести справку
-  -i --input-file           по умолчанию - stdin
-  -o --output-file          по умолчанию - stdout
-  --filters-path            путь до папки, в которой находятся файлы с фильтрами (по умолчанию - ./filters)
-  --filters-pick            (TODO) список применяемых фильтров (если не указан, применяются все фильтры из --filters-path)
-  --interactive             (TODO) интерактивный режим, в котором для каждого символа, который не проходит фильтры, задаётся вопрос пользователю. (Для создания и проверки фильтров)
-  --interactive-new-filter  (TODO) интерактивный режим для создания нового фильтра
+    -h --help                   вывести справку
+    -i --input-file             по умолчанию - stdin
+    -o --output-file            по умолчанию - stdout
+    --filters-path              путь до папки, в которой находятся файлы с фильтрами (по умолчанию - ./filters)
+    --filters-pick              (TODO) список применяемых фильтров (если не указан, применяются все фильтры из --filters-path)
+    --interactive               (TODO) интерактивный режим, в котором для каждого символа, который не проходит фильтры, задаётся вопрос пользователю. (Для создания и проверки фильтров)
+    --interactive-new-filter    ?? (TODO) интерактивный режим для создания нового фильтра
 
 """
 
-from docopt import docopt
-from sys import stdin, stdout
 from os import getcwd
+from sys import stdin, stdout
+
+from docopt import docopt
+
+from utils import join_filters
 
 # преобразование параметров командной строки в dict
 arguments = docopt(__doc__)
 # print(arguments)
 
 # разбираем параметры командной строки
+# входные данные либо из указанного файла, либо с stdin
 if arguments["--input-file"]:
-  input_file = arguments["INPUT_FILE"]
+    with open(arguments["INPUT_FILE"], mode="r") as input_f:
+        input_data = input_f.readlines()
 else:
-  input_file = stdin
+    input_data = stdin
 
-if arguments["--output-file"]:
-  output_file = arguments["OUTPUT_FILE"]
-else:
-  output_file = stdout
+# строка для результатов фильтрации
+output_data = ""
 
+# путь до папки с фильтрами
 if arguments["--filters-path"]:
-  filter_path = arguments["PATH"]
+    filter_path = arguments["PATH"]
 else:
-  filter_path = getcwd() + "/filters"
+    filter_path = getcwd() + "/filters"
 
-
+# собираем все доступные/требуемые фильтры в один set
+all_filters = join_filters(path=filter_path)
 
 # прочитать строку из файла
-# если режим не -ia
+# TODO если режим не --interactive
 # пропустить строку через функцию filter
-# вывести результат
+# записать результат в output_data
+for line in input_data:
+    for item in filter(lambda char: char in all_filters, line):
+        output_data += item
 
-# если режим -ia
+# вывести результат либо в указанный файл, либо в stdout
+if arguments["--output-file"]:
+    with open(arguments["OUTPUT_FILE"], mode="w") as output_file:
+        print(output_data, file=output_file)
+else:
+    print(output_data)
+
+# TODO
+# если режим --interactive
 # каждый символ строки пропустить через функцию filter
 # если символ не прошёл фильтр
 # задать вопрос пользователю: сохранить ли символ в фильтре
 # если ответ - y
 # сохранить символ во временный фильтр
 # в конце сохранить временный фильтр в новый файл или добавить временный фильтр в существующий файл
-
